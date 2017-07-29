dc1a48dad5d038fe5c11624d1acc34b57592877c
Admin-PC\Admin <whuxugang@aliyun.com>
2017-07-29 11:30:57 +0800

    pdqn自己训练

--- game/rlutil.py
+++ game/rlutil.py
@@ -74,11 +74,39 @@ def combine(s, a):
         s[33+i] = 1
     return s
     
+############################################
+#                  改变scope               #
+############################################      
     
-    
-    
-    
-    
+def rename(checkpoint_dir, replace_from, replace_to, add_prefix, dry_run=False):
+    checkpoint = tf.train.get_checkpoint_state(checkpoint_dir)
+    with tf.Session() as sess:
+        for var_name, _ in tf.contrib.framework.list_variables(checkpoint_dir):
+            # Load the variable
+            var = tf.contrib.framework.load_variable(checkpoint_dir, var_name)
+
+            # Set the new name
+            new_name = var_name
+            if None not in [replace_from, replace_to]:
+                new_name = new_name.replace(replace_from, replace_to)
+            if add_prefix:
+                new_name = add_prefix + new_name
+
+            if new_name == var_name:
+                continue
+
+            if dry_run:
+                print('%s would be renamed to %s.' % (var_name, new_name))
+            else:
+                print('Renaming %s to %s.' % (var_name, new_name))
+                # Rename the variable
+                var = tf.Variable(var, name=new_name)
+
+        if not dry_run:
+            # Save the variables
+            saver = tf.train.Saver()
+            sess.run(tf.global_variables_initializer())
+            saver.save(sess, checkpoint.model_checkpoint_path)    
 
         
     
